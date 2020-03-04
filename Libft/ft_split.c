b463e35e3aa73408ad962ae1351489d4ac2560f1
Iroh <marvin@42.fr>
2020-03-04 01:36:34 -0300

    Part2 ft_itoa

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,61 +6,56 @@
 /*   By: mtaiar-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 15:07:34 by mtaiar-s          #+#    #+#             */
-/*   Updated: 2020/03/02 20:14:46 by mtaiar-s         ###   ########.fr       */
+/*   Updated: 2020/03/03 20:52:16 by mtaiar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 #include "libft.h"
+
+int     nextC(char const *s, char c, int i)
+{
+    int     j;
+
+    j = 0;
+    while (s[i + j] != c && s[i + j] != '\0')
+        j++;
+    return (j);
+}
 
 char    **ft_split(char const *s, char c)
 {
     size_t  i;
     size_t  j;
     size_t  size;
-    size_t  lst;
-    size_t  crepeat;
     char    **strs;
 
     if (!s)
         return (NULL);
     i = -1;
-    size = 1;
+    size = 0;
     while (s[++i])
     {
-        if (s[i] == c && s[i + 1] != c)
+        if ((s[i] != c && s[i - 1] == c))
             size++;
     }
     if (!(strs = malloc(size * sizeof(char *) + 1)))
         return (NULL);
-    i = -1;
+    i = 0;
     j = 0;
-    lst = 0;
-    crepeat = 0;
-    while (s[++i])
+    while (s[i] && j <= size)
     {
-        if (s[i] == c && s[i + 1] == c)
-            crepeat++;
-        else if (j == 0 && s[i] == c && s[i + 1] != c)
+        while (s[i] == c)
         {
-            strs[j] = ft_substr(s, 0, i - crepeat);
-            lst = i;
-            crepeat = 0 * j++;
+            i++;
         }
-        else if (s[i] == c && s[i + 1] != c)
-        {  
-            strs[j] = ft_substr(s, lst + 1, i - lst - 1 - crepeat);
-            lst = i;
-            crepeat = 0 * j++;
+        if (s[i] != c)
+        {
+            strs[j] = ft_substr(s, i, nextC(s, c, i));
+            j++;
+            i += nextC(s, c, i);
         }
-        else if (s[i + 1] == '\0')
-        {  
-            strs[j] = ft_substr(s, lst + 1, i - lst - crepeat);
-            lst = i;
-            crepeat = 0 * j++;
-        }
+        i++;
     }
-    strs[j + 1] = '\0';
+    strs[j] = '\0';
     return (strs);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mtaiar-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 15:02:14 by mtaiar-s          #+#    #+#             */
-/*   Updated: 2020/03/02 17:27:10 by mtaiar-s         ###   ########.fr       */
+/*   Updated: 2020/03/03 22:36:17 by mtaiar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
 char    *ft_strtrim(char const *s1, char const *set);
+char    *ft_itoa(int n);
 char    **ft_split(char const *s, char c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
